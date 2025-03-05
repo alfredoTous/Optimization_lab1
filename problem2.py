47a7d9a899b8fc2f7e18a6baff5859b05f46b05f
alfredoTous <adtous@uninorte.edu.co>
2025-03-05 14:07:55 -0500

    feat: added Time Comparation Method

--- problem2.py
+++ problem2.py
@@ -2,16 +2,38 @@
 
 from scipy.sparse import csr_matrix, csc_matrix, coo_matrix
 import numpy as np
+import time
 
-# Matriz densa de ejemplo
+#GLOBAL VARIABLES
 dense_matrix = [
-    [0, 0, 3, 0],
-    [2, 0, 0, 0],
-    [0, 7, 0, 0],
-    [0, 0, 0, 5]
+    [0, 2, 0, 0, 1],
+    [3, 0, 0, 4, 0],
+    [0, 0, 5, 0, 0],
+    [0, 6, 0, 0, 7],
+    [8, 0, 0, 9, 0]
 ]
 
-def coo_matrix_self(matrix):
+dense_matrix_2 = [
+    [0, 4, 0, 0, 0],
+    [0, 0, 0, 6, 0],
+    [7, 0, 0, 0, 0],
+    [0, 0, 2, 0, 8],
+    [0, 0, 0, 3, 0]
+]
+
+class Coo_matrix_self:
+
+    def __init__(self,v,i,j):
+        self.v = v
+        self.i = i
+        self.j = j
+    
+    def sum(self,dense_matrix_to_sum):
+        converted_dense_matrix = build_coo_matrix_self(dense_matrix_to_sum)
+
+
+
+def build_coo_matrix_self(matrix):
     v=[]
     i=[]
     j=[]
@@ -23,15 +45,38 @@ def coo_matrix_self(matrix):
                 v.append(num)
                 i.append(row_counter)
                 j.append(row.index(num))
-                print(f"\n[!]num:{num}, row:{row_counter}, column: {row.index(num)}")
-    print(v)
-    print(i)
-    print(j)
+    print("v = ",v)
+    print("i = ",i)
+    print("j = ",j)
+    coo_matrix_self = Coo_matrix_self(v,i,j)
+    return coo_matrix_self
+
+def compare_building_time(matrix):
+    start_time_self = time.time()
+    coo_matrix_self = build_coo_matrix_self(matrix)
+    end_time_self = time.time()
+    
+    start_time_library = time.time()
+    coo_matrix_library = coo_matrix(dense_matrix)
+    end_time_library = time.time()
+
+    total_time_self = end_time_self - start_time_self
+    total_time_library = end_time_library - start_time_library
+
+    return total_time_self, total_time_library
+
+
+
+build_coo_matrix_self(dense_matrix)
+print("\n\n")
+build_coo_matrix_self(dense_matrix_2)
+
+
 #coo = coo_matrix(dense_matrix)
 
 #csr = csr_matrix(dense_matrix)
 
 #csc = csc_matrix(dense_matrix)
 
-coo_matrix_self(dense_matrix)
+
 
